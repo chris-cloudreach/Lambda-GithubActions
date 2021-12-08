0a3a7b1f251779c4e10ab676cb810e494590246a
Christopher <christopher.ogbunuzor@cloudreach.com>
2021-12-08 12:25:59 +0000

    Try again

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3"{
       bucket = "my-first-lambdabucket-194694014750"
-      key = "sprint3/week6/lambda-function/terraform.tfstate"
+      key = "sprint3/week7/lambda-function-githubaction/terraform.tfstate"
       dynamodb_table = "terraform-lock"
       
   }
