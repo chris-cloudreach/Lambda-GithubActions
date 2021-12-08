2ba4d1871b1504ef9d62ae618923bd2741ef8338
Christopher <christopher.ogbunuzor@cloudreach.com>
2021-12-08 10:54:59 +0000

    Tring again

--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3"{
       bucket = "my-first-lambdabucket-194694014750"
       key = "sprint3/week6/lambda-function/terraform.tfstate"
-      # dynamodb_table = "terraform-lock"
+      dynamodb_table = "terraform-lock"
+      
   }
 }
